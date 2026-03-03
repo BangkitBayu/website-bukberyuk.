04ed295b4d8766e870467dd642060b584a4c6bf4
BangkitBayu <martinusmartinussaja@gmail.com>
2026-03-03 21:12:30 +0700

    fix: layouting in header

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const navToggle = document.getElementById("nav-toggle");
 const mobileNav = document.getElementById("mobile-nav");
+const desktopNav = document.getElementById("desktop-nav");
+const header = document.querySelector("header");
 
 navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
-  mobileNav.classList.toggle("d-none")
-  document.body.classList.toggle("active")
+  mobileNav.classList.toggle("d-none");
+  document.body.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 200) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
 });
